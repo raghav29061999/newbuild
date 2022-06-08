89a2f606639aa4ee36e3322b4b66ed760c5fb09c
raghav <singhalraghav.59@hotmail.com>
2022-06-08 17:40:40 +0530

    test runner

--- src/test/java/StepDefinitions/TestRunner.java
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -1,5 +1,15 @@
 package StepDefinitions;
 
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/Features", glue= {"StepDefinitions"}, monochrome = true, plugin = {"pretty"
+		, "html:target/html1"
+})
+
 public class TestRunner {
 
 }
